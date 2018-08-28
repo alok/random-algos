dd1ba11de42e14894cc4a5f606c09b29c3ce06f6
Alok Singh <alokbeniwal@gmail.com>
2018-08-27 21:37:51 -0700

    Fix contraction algorithm
    1. Use list instead of set to track multiple edges
    2. Correctly implement contraction logic.

--- min_cut.py
+++ min_cut.py
@@ -24,11 +24,11 @@ def gen_sym(V: int = 10) -> np.ndarray:
 
 
 def gen_adj_list(G: AdjacencyMatrix) -> AdjacencyList:
-    adj = {i: set() for i, _ in enumerate(G)}
+    adj = {i: [] for i, _ in enumerate(G)}
     for v, row in enumerate(G):
         for c, vertex in enumerate(row):
             if row[c] == 1:
-                adj[v].add(c)
+                adj[v].append(c)
     return adj
 
 
@@ -39,16 +39,39 @@ def select_edge(A: AdjacencyList) -> Edge:
     return random.choice(edges)
 
 
-# TODO rm deepcopy and do this in place
-def contract_edge(edge: Edge, A: AdjacencyList) -> AdjacencyList:
-    v, w = edge
-    A = deepcopy(A)
+def contract_edge(edge: Edge, A: AdjacencyList) -> None:
     # add w's connections to v
-    A[v].update(A[w])
+
+    # # rm w from all connections
+    # for vertex in A:
+    #     while w in A[vertex]:
+    #         A[vertex].remove(w)
+
+    # # A[v].extend([u for u in A[w] if u != v])
+    # A[v].extend([u for u in A[w] if u != v])
+    # del A[w]
+
+    v, w = edge
+
+    # rm *all* edges between v and w
+    while w in A[v]:
+        A[v].remove(w)
+    while v in A[w]:
+        A[w].remove(v)
+
+    # No self edges to worry about by assumption/construction above.
+    A[v].extend(A[w])
+
     del A[w]
-    # rm w from all connections
+
+    #
     for vertex in A:
-        A[vertex].discard(w)
+        for i, u in enumerate(A[vertex]):
+            if u == w:
+                A[vertex][i] = v
+
+
+
 
     return A
 
